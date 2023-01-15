aa508af4d576602e0566c8011c9b2e5f8502e83f
maveen <maveen@Julias-MacBook.fritz.box>
2023-01-15 01:30:32 +0100

    first commit

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -2,6 +2,7 @@ package com.example.application;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +15,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(value = "flowcrmtutorial")
+@Theme(value = "connectcrm")
+@PWA(
+        name = "ConnectCRM",
+        shortName = "CRM",
+        offlinePath = "offline.html",
+        offlineResources = {"images/logo.png","images/offline.png"}
+)
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application implements AppShellConfigurator {
 
